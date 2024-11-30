8a6e63b18861a832b100de3aabcbbc9d34e582d3
GoldenQueens <goldenqueenmd@gmail.com>
2024-11-30 13:22:19 +0530

    Update captain.js

--- captain.js
+++ captain.js
@@ -1,5 +1,5 @@
 {
-  "alive": "*SUPPORT US *\n\n_Main Channel 🧚 :_\n_Youtube :_\n_web Site :_\n\n*DEVELOPERS 👤*\n\n_Mr Vima_\nhttps://wa.me/94776734030?text=VIMAMODS\n\n_Mr Dila_\nhttps://wa.me/94777839446?text=dila\n\n_Mr Rocky_\nhttps://wa.me/94771665143?text=Rocky\n\n♞🧚👺👺🥳🕐🥰",
+  "alive": "🥰🥰🥰🥰",
   "menu": "hy\nmy\n\nhi",
  "previewlink": "https://whatsapp.com/channel/0029Vaw0NYoAInPkQUJJZK1t",
     "vimanumber":"94776734030",
