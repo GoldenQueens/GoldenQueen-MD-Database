618a97d7da6afbe273a4a541cf43645bb8453402
Golden Queen MD <goldenqueenmd@gmail.com>
2024-12-17 15:48:26 +0530

    Update value.js

--- value.js
+++ value.js
@@ -1,8 +1,11 @@
 {
 "states_save_words":"send,එවපන්,sa",
+
 "link_words":"nanan,njsj,nanan,nNan",
-  "paid_numbers":"94777839446,94776734030,94771665143,94720319480",
-  "status_text": [
+
+"paid_numbers":"94777839446,94776734030,94771665143,94720319480",
+
+"status_text": [
   "Boot status sinhala", 
   "sinhala wadan", 
   "yamaha 11 sri lanka", 
