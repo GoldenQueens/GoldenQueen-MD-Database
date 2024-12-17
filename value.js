2cc1b1f6d4bbb049d94119ab18ca9e74dd569254
Golden Queen MD <goldenqueenmd@gmail.com>
2024-12-17 15:16:58 +0530

    Update value.js

--- value.js
+++ value.js
@@ -1,7 +1,9 @@
 {
 "states_save_words":"send,එවපන්,sa",
 "link_words":"nanan,njsj,nanan,nNan",
-  "paid_numbers":"94777839446,94776734030,94771665143,94720319480"
+  "paid_numbers":"94777839446,94776734030,94771665143,94720319480",
+  "status_text":"1,2,3,4,5"
+
 
 
 
