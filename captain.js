101dff0aae73d24daa97f19e13ad862d1c15ab3b
Golden Queen MD <goldenqueenmd@gmail.com>
2025-01-19 19:50:24 +0530

    Update captain.js

--- captain.js
+++ captain.js
@@ -7,7 +7,7 @@
     "dilanumber":"94777839446",
     "rockynumber":"94704104383",
     "deploymessageowners":"*Hey Owners 🧚*\n*I deployed Golden Queen MD 🥳*",
-    "zipmegalink" : "https://mega.nz/file/1AIlhYIQ#PC9AKO0JUPCxAMq8I0pLCzwCG783zPslMRm3Q1cgwXw",
+    "zipmegalink" : "https://mega.nz/file/kUxmlCgQ#nyjOSPCMuIAOCk37xxNnPppH4vPRmkhsFifxIXRPbfs",
     "developerNumber" : "94776734030",
 "proNumber" : "94776734030@s.whatsapp.net,94777839446@s.whatsapp.net,94720319480@s.whatsapp.net,94743319446@s.whatsapp.net"
 }
