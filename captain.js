539c87814acdedc5eeeb68fb80919a614835392b
Golden Queen MD <goldenqueenmd@gmail.com>
2025-01-22 02:58:54 +0530

    Update captain.js

--- captain.js
+++ captain.js
@@ -1,13 +1,13 @@
 {
   "alive": " ",
- "update": "\n\n`➕Added` : Status View All Seve Member One Time\n`➕Added` : Groups Auto Sender Status Video\n`➕Added` : Groups Auto Sender Song Audio\n`➕Added` : Groups Auto Sender Wallpepar\n`➕Added` : Groups Auto Sender Xnxx Video\n➕Added` : Fast & Easy Settings Changes\n`🛠️Fixed` : Video Command Video Downloaded\n`🛠️Fixed` : Sticker Command Sticker Converter\n`🛠️Fixed` : Auto React♥️ Bug Fix\n",
+ "update": "\n\n`➕Added` : Status View All Seve Member One Time\n`➕Added` : Auto Bock All,Command only, Warn, Command Warn Costomize\n`🛠️Fixed` : Video Command Video Downloaded\n`🛠️Fixed` : Song & Video Comand\n`🛠️Fixed` : Auto React♥️ Bug Fix\n",
   "menu": "hy\nmy\n\nhi",
  "previewlink": "https://whatsapp.com/channel/0029Vaw0NYoAInPkQUJJZK1t",
     "vimanumber":"94776734030",
     "dilanumber":"94777839446",
     "rockynumber":"94704104383",
     "deploymessageowners":"*Hey Owners 🧚*\n*I deployed Golden Queen MD 🥳*",
-    "zipmegalink" : "https://mega.nz/file/kRpRGI4a#GCIWCiDR_iMGfSlSrxInQlrjy0KBJnZBDIwdpiw567Q",
+    "zipmegalink" : "https://mega.nz/file/dJw3QBbB#vh_b4-JMaguzLmvxQgLEJlMjRRTZGyq-3_-Bxgfe2Ig",
     "developerNumber" : "94776734030",
 "proNumber" : "94776734030@s.whatsapp.net"
 }
