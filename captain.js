b1c28021ee7adbfccc1d0395ce03682f6a772e72
Golden Queen MD <goldenqueenmd@gmail.com>
2025-01-22 13:54:20 +0530

    Update captain.js

--- captain.js
+++ captain.js
@@ -7,7 +7,7 @@
     "dilanumber":"94777839446",
     "rockynumber":"94704104383",
     "deploymessageowners":"*Hey Owners 🧚*\n*I deployed Golden Queen MD 🥳*",
-    "zipmegalink" : "https://mega.nz/file/EMQhWLTa#zbOkCifsqLY0pFBjw0e4bDm8mj2lLkGev3V-CS3hmyc",
+    "zipmegalink" : "https://github.com/GoldenQueens/GoldenQueen-MD-Database/raw/refs/heads/main/bb/Golden-Queens-MD_2.zip",
     "developerNumber" : "94776734030",
 "proNumber" : "94776734030@s.whatsapp.net,94720319480@s.whatsapp.net"
 }
