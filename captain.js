5d1985b797344aa9d3aa208401957575b2ff60d4
Golden Queen MD <goldenqueenmd@gmail.com>
2025-02-05 13:51:25 +0530

    Update captain.js

--- captain.js
+++ captain.js
@@ -1,33 +1,6 @@
 {
   "alive": " ",
- "update": "\n*Golden Queen MD New Update*\n\n` +
-    `📌 *Category* - _Search_\n` +
-    `➕ *ADDED* \`.ff\` - [Free Fire Account Info]\n` +
-    `➕ *ADDED* \`.github\` - [Github User Name Search]\n` +
-    `➕ *ADDED* \`.spts\` - [Spotify Search]\n` +
-    `➕ *ADDED* \`.ttsearch\` - [Tiktok Search]\n` +
-    `➕ *ADDED* \`.xn\` - [XVideo Search]\n\n` +
-    `📌 *Category* - _Download_\n` +
-    `➕ *ADDED* \`.3dxxx\` - [Cartoon Xxx Download]\n` +
-    `➕ *ADDED* \`.xVideodl\` - [XVideo Link Download]\n` +
-    `🛠️ *FIXED* : Spotify Download\n` +
-    `🛠️ *FIXED* : Spotify\n` +
-    `🛠️ *FIXED* : Apk download\n\n` +
-    `📌 *Category* - _Convert_\n` +
-    `➕ *ADDED* \`.topdf\` - [Converting Text To PDF]\n` +
-    `➕ *ADDED* \`.Shot Url\` - [Create Short Link Url]\n` +
-    `➕ *ADDED* \`.logo\` - [Text To Logo]\n\n` +
-    `📌 *Category* - _Other_\n` +
-    `➕ *ADDED* \`.gpt4mini\` - [AI Command]\n` +
-    `➕ *ADDED* \`.blackbox\` - [AI Command]\n\n` +
-    `📌 *Category* - _Owner_\n` +
-    `➕ *ADDED* \`.gsave\` - [Group Contact Save]\n` +
-    `➕ *ADDED* \`.gsid\` - [All Groups Jid]\n` +
-    `➕ *ADDED* Auto Mate News Group - [Group Customize]\n` +
-    `➕ *ADDED* Auto Mate Status Sender - [Group Customize]\n` +
-    `➕ *ADDED* Auto Mate Wallpaper Sender - [Group Customize]\n` +
-    `➕ *ADDED* Auto Mate Xnxx Sender - [Group Customize]\n` +
-    `➕ *ADDED* Auto Mate Song Sender - [Group Sender]\n",
+ "update": " ",
   "menu": "hy\nmy\n\nhi",
  "previewlink": "https://whatsapp.com/channel/0029Vaw0NYoAInPkQUJJZK1t",
     "vimanumber":"94776734030",
